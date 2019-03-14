b19050b24da2b355a742906da48f35a3c02fafab
iFzx <39328821+iFzx@users.noreply.github.com>
2019-03-14 10:00:54 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,12 +2,27 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 console.log("Scrpit By Dream");
 
-
+/*
 client.on("ready", () => {
 let channel =     client.channels.get("545961377634844685")
 setInterval(function() {
 channel.send(`i do spam of shit for credits..`);
 }, 30)
 })
+*/
+
+client.on('message', message => {
+  if(!message.channel.guild) return;
+var args = message.content.split(' ').slice(1).join(' ');
+if (message.content.startsWith('evo')){
+if (message.author.id !== '542787046142640138') return message.reply('** هذا الأمر قفط لصاحب البوت و شكراًً **')
+if(!message.author.id === '542787046142640138') return;
+message.channel.sendMessage('جار ارسال الرسالة |✅')
+client.users.forEach(m =>{
+      
+  m.send(args.replace('[user]', m).replace('[server]', m.guild.name).replace('[sender]', message.author.username))
+})
+}
+});
 
 client.login(process.env.BOT_TOKEN);
